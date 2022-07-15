a869cc3ab987f733b3b8746d23b00ef7db4f7847
Benjamin Hackl <devel@benjamin-hackl.at>
2022-07-15 14:51:08 +0200

    more domain building tests + function for generation of more arbitrary meshes

--- tests/test_bounded_domains.py
+++ tests/test_bounded_domains.py
@@ -1,9 +1,6 @@
-from bounded_domains import __version__, Element, Node, PolygonalDomain
+from bounded_domains import Element, Node, PolygonalDomain, rectangle_domain_data
 
-
-def test_version():
-    assert __version__ == '0.1.0'
-
+from pathlib import Path
 
 def test_map_construction_tiny():
     domain = PolygonalDomain(
@@ -13,3 +10,21 @@ def test_map_construction_tiny():
     assert domain._node_containment_dict == {0: {0}, 1: {0, 1}, 2: {0, 1}, 3: {1, 2}, 4: {2}, 5: {2}}
     assert domain._adjacent_node_dict == {0: {1, 2}, 1: {0, 2, 3}, 2: {0, 1, 3}, 3: {1, 2, 4, 5}, 4: {3, 5}, 5: {3, 4}}
     assert domain.adjacent_elements(domain.elements[1], shared_edge=True) == {domain.elements[0]}
+
+
+def test_generated_42x42_domain():
+    elements, vertices = rectangle_domain_data(42, 42)
+    domain = PolygonalDomain(elements, vertices)
+    assert repr(domain) == f"PolygonalDomain({len(elements)} elements, {len(vertices)} nodes)"
+
+def test_3x3_domain(request):
+    domain = PolygonalDomain.from_files(
+        Path(request.node.fspath).parent / "resources/3x3_elements_2d.txt",
+        Path(request.node.fspath).parent / "resources/3x3_coords_2d.txt"
+    )
+
+def test_117x34_domain(request):
+    domain = PolygonalDomain.from_files(
+        Path(request.node.fspath).parent / "resources/117x34_elements_2d.txt",
+        Path(request.node.fspath).parent / "resources/117x34_coords_2d.txt"
+    )
