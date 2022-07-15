497e1b11147998e8b81055dc380e10a11d7751e3
Benjamin Hackl <devel@benjamin-hackl.at>
2022-07-15 05:05:32 +0200

    implemented generation of mappings

--- bounded_domains/structures.py
+++ bounded_domains/structures.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 
-from collections import namedtuple
+from typing import Iterable
+
+from collections import namedtuple, defaultdict
 from pathlib import Path
 
 
@@ -12,8 +14,18 @@ from . import logger
 
 class Element:
     """Representation of a (triangular) element."""
-    def __init__(self, *vertex_ids) -> None:
-        self.vertices = set(vertex_ids)
+    def __init__(self, id: int, vertices: Iterable[int]) -> None:
+        self.id = id
+        vertices = set(vertices)
+        if len(vertices) != 3:
+            raise ValueError(
+                f"Element with index {id} is not a triangle; "
+                f"passed vertex indices: {vertices}"
+            )
+        self.vertices = vertices
+
+    def __repr__(self):
+        return f"Element(id={self.id}, vertices={self.vertices})"
 
 
 Node = namedtuple('Node', ['x', 'y'])
@@ -22,7 +34,7 @@ Node = namedtuple('Node', ['x', 'y'])
 
 class PolygonalDomain:
     """A polygonal domain consisting of (triangular) elements.
-    
+
     Parameters
     ----------
     elements
@@ -55,11 +67,43 @@ class PolygonalDomain:
 
     def build_mappings(self) -> None:
         """Iterate through domain elements and build auxiliary relation dictionaries."""
-        self._node_containment_dict = None
-        self._adjacent_element_dict = None
-        self._adjacent_node_dict = None
+        self._node_containment_dict = defaultdict(set)
+        self._adjacent_elements_shared_vertex_dict = defaultdict(set)
+        self._adjacent_elements_shared_edge_dict = defaultdict(set)
+        self._adjacent_node_dict = defaultdict(set)
 
-        # TODO: iterate through element list once, build dictionaries.
+        for element in self.elements:
+            for vertex in element.vertices:
+                self._node_containment_dict[vertex].add(element.id)
+
+        logger.debug("Populated node containment dictionary.")
+
+        # Given an element E, all adjacent elements can be
+        # determined by considering the union of all elements
+        # the vertices of E are contained in. The edge-adjacent
+        # ones can then be filtered out.
+
+        for element in self.elements:
+            for vertex in element.vertices:
+                adjacent_elements = self.elements_containing_vertex(vertex)
+                self._adjacent_elements_shared_vertex_dict[element].update(adjacent_elements)
+                for adjacent_element_id in adjacent_elements:
+                    adjacent_element = self.elements[adjacent_element_id]
+                    self._adjacent_node_dict[vertex].update(adjacent_element.vertices)
+
+            self._adjacent_elements_shared_vertex_dict[element].remove(element.id)
+
+            edge_adjacent_elements = {
+                self.elements[id] for id
+                in self._adjacent_elements_shared_vertex_dict[element]
+                if len(set.intersection(element.vertices,
+                                        self.elements[id].vertices)) == 2
+            }
+            self._adjacent_elements_shared_edge_dict[element] = edge_adjacent_elements
+
+        for vertex, neighbors in self._adjacent_node_dict.items():
+            neighbors.remove(vertex)
+
 
     def build_quadtree(self) -> None:
         """Iterate through domain vertices and build auxiliary quadtree."""
@@ -70,15 +114,21 @@ class PolygonalDomain:
     def elements_containing_vertex(self, vertex_index: int) -> list[Element]:
         return self._node_containment_dict[vertex_index]
 
-    def adjacent_elements(self, element: Element) -> list[Element]:
-        return self._adjacent_element_dict[element]
+    def adjacent_elements(
+        self,
+        element: Element,
+        shared_edge: bool = False
+        ) -> list[Element]:
+        if shared_edge:
+            return self._adjacent_elements_shared_edge_dict[element]
+        return self._adjacent_elements_shared_vertex_dict[element]
 
     def adjacent_vertices(self, vertex_index: int) -> list[int]:
         return self._adjacent_node_dict[vertex_index]
 
     def closest_element(self, node: Node, return_index: bool = False) -> Element | int:
         """Determines a element of the domain that is closest to the specified node.
-        
+
         Parameters
         ----------
         node
